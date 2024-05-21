4276d8188eb56084137cf812914cf60d9816f075
Suvigya2008 <bhandarisuvigya2008@gmail.com>
2024-05-21 10:23:03 +0545

    first commit

---
+++ project.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+url = "https://books.toscrape.com/"
+html = requests.get(url).content
+
+soup = BeautifulSoup(html,"html.parser")
+book_html = soup.find_all("h3")
+book = [item.text for item in book_html]
+price_html = soup.find_all(class_="price_color")
+price = [i.text for i in price_html]
+availability_html = soup.find_all(class_="instock availability")
+availability = [k.text for k in availability_html]
+dictionary = {
+    "book":book,"price":price,"Availability":availability
+}
+df = pd.DataFrame(dictionary)
+print(df)
---
+++ while.py
@@ -0,0 +1,7 @@
+nums = [8,6,7,2]
+index = 0 
+
+while index <len(nums):
+    item = nums[index]
+    print(item)
+    index = index + 1
